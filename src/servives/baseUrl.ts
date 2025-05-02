44756860d3e5316b8b047ef2f6a8b104966f41b5
MacBookPro <lezie04@gmail.com>
2025-05-02 11:19:07 -0700

    BASE_URL_PROD

--- src/servives/baseUrl.ts
+++ src/servives/baseUrl.ts
@@ -9,11 +9,11 @@ const BASE_URL_SITE_PROD ='https://ms.cloud.tarafe.com/catalogue';
 const BASE_URL_SITE ='https://tarafe.com/catalogue';
 
 export const getBaseUrl = (): string => {
-    return BASE_URL_DEV;
+    return BASE_URL_PROD;
 };
 
 export const getBaseUrlImg = (): string => {
-    return BASE_URL_IMGAE_DEV;
+    return BASE_URL_IMGAE_PROD;
 };
 export const getBaseSiteUrl = (): string => {
     return BASE_URL_SITE;
